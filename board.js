5456cb6ac02e8e13b15930cca9b25c73fed8c967
jabo landry <arnoldjabo@gmail.com>
2024-04-15 16:20:47 +0200

    new project

---
+++ board.js
@@ -0,0 +1,17 @@
+let btns=document.querySelectorAll("button")
+
+btns.forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        // document.querySelector(".active")?.classList.toggle("active")
+       let pragraph=btn.dataset.target
+       let see=document.getElementById(pragraph)
+       see.classList.toggle("active")
+       if(see.classList.contains("active")){
+        btn.innerHTML="-"
+       }else{
+        btn.innerHTML="+"
+       }
+     
+    })
+
+}) 
