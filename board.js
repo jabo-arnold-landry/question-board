cd41fe69bbdc74370c47e0ee641aaef68e74828f
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-06 15:03:34 +0200

    fix:Fix the alignment of the codebase

--- board.js
+++ board.js
@@ -1,17 +1,15 @@
-let btns=document.querySelectorAll("button")
+let btns = document.querySelectorAll("button");
 
-btns.forEach(btn=>{
-    btn.addEventListener("click",()=>{
-        // document.querySelector(".active")?.classList.toggle("active")
-       let pragraph=btn.dataset.target
-       let see=document.getElementById(pragraph)
-       see.classList.toggle("active")
-       if(see.classList.contains("active")){
-        btn.innerHTML="-"
-       }else{
-        btn.innerHTML="+"
-       }
-     
-    })
-
-}) 
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // document.querySelector(".active")?.classList.toggle("active")
+    let pragraph = btn.dataset.target;
+    let see = document.getElementById(pragraph);
+    see.classList.toggle("active");
+    if (see.classList.contains("active")) {
+      btn.innerHTML = "-";
+    } else {
+      btn.innerHTML = "+";
+    }
+  });
+});
