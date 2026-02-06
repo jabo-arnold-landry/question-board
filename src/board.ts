ff09f4e2f18410cba1ccdefbda5c65bcb822e50a
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-06 16:51:49 +0200

    feat:Implement a way to toggle a single pragraph when we a user decides to change the question

--- src/board.ts
+++ src/board.ts
@@ -2,7 +2,9 @@ let btnsContainer = document.querySelector(".box") as HTMLDivElement;
 
 btnsContainer.addEventListener("click", (e: Event) => {
   if (e.target instanceof HTMLButtonElement) {
+    const pragraphs = document.querySelectorAll("p");
+    pragraphs.forEach((pElement) => pElement.classList.remove("active"));
     const pragraph = document.getElementById(`${e.target.dataset.target}`);
-    pragraph?.classList.toggle(".active");
+    pragraph?.classList.toggle("active");
   }
 });
