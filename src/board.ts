2541852b60886cef440ae3df3c35a3fde176f53b
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-07 14:17:00 +0200

    fix: Add a toggler to show active pragraph based on the choice the user have made

--- src/board.ts
+++ src/board.ts
@@ -1,4 +1,5 @@
 let btnsContainer = document.querySelector(".box") as HTMLDivElement;
+const btns = document.querySelectorAll("button");
 
 btnsContainer.addEventListener("click", (e: Event) => {
   if (e.target instanceof HTMLButtonElement) {
@@ -6,5 +7,15 @@ btnsContainer.addEventListener("click", (e: Event) => {
     pragraphs.forEach((pElement) => pElement.classList.remove("active"));
     const pragraph = document.getElementById(`${e.target.dataset.target}`);
     pragraph?.classList.toggle("active");
+    let indexOfClickedBtn = [...btns].indexOf(e.target);
+    activeButton(indexOfClickedBtn);
   }
 });
+
+function activeButton(index = 0): void {
+  btns.forEach((btn) => (btn.textContent = "-"));
+  const activeBtn = btns[index] as HTMLButtonElement;
+  activeBtn.textContent = "+";
+}
+
+activeButton();
